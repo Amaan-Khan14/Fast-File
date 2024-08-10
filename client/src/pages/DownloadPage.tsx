9f8ae4a6f3bed4856a2abfde5201d82b8c6015b1
Amaan-Khan14 <amaank14@icloud.com>
2024-08-10 17:16:22 +0530

    Auth Done

--- client/src/pages/DownloadPage.tsx
+++ client/src/pages/DownloadPage.tsx
@@ -81,6 +81,7 @@ export default function DownloadPage() {
             })
         }
     };
+
     const handleDownload = async () => {
         if (fileInfo && encryptionKey) {
             const response = await fetch(fileInfo.url);
@@ -155,23 +156,23 @@ export default function DownloadPage() {
     return (
         <div className='bg-gradient-to-b from-[#090a15] via-[#0b1d23] to-[#090a15] min-h-screen'>
             <AppBar />
-            <div className="mt-44 mb-10 mx-10 flex items-center justify-center">
-                <Card className="bg-inherit p-5 rounded-lg w-full max-w-5xl border-[#b7f4ee]">
+            <div className="mt-20 sm:mt-44 mb-10 mx-4 sm:mx-10 flex items-center justify-center">
+                <Card className="bg-inherit p-3 sm:p-5 rounded-lg w-full max-w-5xl border-[#b7f4ee] mb-10">
                     <CardContent>
-                        <div className="grid grid-cols-2 gap-8">
-                            <CardHeader>
+                        <div className="flex flex-col sm:flex-row gap-8">
+                            <CardHeader className="w-full sm:w-1/2">
                                 <div>
-                                    <div className="flex justify-between gap-4 items-center bg-[#16a394] p-3 rounded mb-4">
+                                    <div className="flex justify-between gap-2 sm:gap-4 items-center bg-[#16a394] p-2 sm:p-3 rounded mb-4">
                                         <div className="flex items-center w-full">
                                             <span className="mr-2">📷</span>
                                             <Input
                                                 type="text"
                                                 value={fileName}
                                                 readOnly
-                                                className="flex-grow bg-inherit focus-visible:ring-0 focus-visible:ring-offset-0 border-0 text-base text-white p-2 rounded-l"
+                                                className="flex-grow bg-inherit focus-visible:ring-0 focus-visible:ring-offset-0 border-0 text-sm sm:text-base text-white p-1 sm:p-2 rounded-l"
                                             />
                                         </div>
-                                        <span className="text-white">{size
+                                        <span className="text-white text-sm sm:text-base">{size
                                             ? size < 1024
                                                 ? `${size} B`
                                                 : size < 1024 * 1024
@@ -186,51 +187,50 @@ export default function DownloadPage() {
                                 </div>
                             </CardHeader>
 
-
-                            <div>
-                                <p className="my-5">
-                                    <h1 className="text-4xl font-bold bg-gradient-to-r from-teal-500 to-teal-800 bg-clip-text text-transparent">You've got a file!</h1>
-                                </p>
-                                <p className="text-white font-medium text-xl my-2">Share this file</p>
+                            <div className="w-full sm:w-1/2">
+                                <div className="sm:my-5">
+                                    <h1 className="text-2xl sm:text-4xl font-bold bg-gradient-to-r from-teal-500 to-teal-800 bg-clip-text text-transparent">You've got a file!</h1>
+                                </div>
+                                <p className="text-white font-medium text-lg sm:text-xl my-2">Share this file</p>
                                 <div className="flex mb-4 gap-2">
                                     <Input
                                         type="text"
                                         value={window.location.href}
                                         readOnly
-                                        className="flex-grow text-base bg-[#0b1d23] focus-visible:ring-0 focus-visible:ring-offset-1 focus-visible:ring-offset-gray-600 border border-gray-700 text-white p-2 rounded-l"
+                                        className="flex-grow text-sm sm:text-base bg-[#0b1d23] focus-visible:ring-0 focus-visible:ring-offset-1 focus-visible:ring-offset-gray-600 border border-gray-700 text-white p-1 sm:p-2 rounded-l"
                                     />
-                                    <Button onClick={copyToClipboard} className="bg-inherit border w-auto border-[#04c8bb] text-[#04c8bb] hover:text-[#92efe6] hover:border-[#92efe6] font-semibold hover:bg-inherit">
+                                    <Button onClick={copyToClipboard} className="bg-inherit border w-auto border-[#04c8bb] text-[#04c8bb] hover:text-[#92efe6] hover:border-[#92efe6] font-semibold hover:bg-inherit text-sm sm:text-base">
                                         Copy link
                                     </Button>
                                 </div>
                                 <div className="w-full mb-4">
-                                    <Button onClick={toggleQRCode} className="bg-inherit border w-full border-[#04c8bb] text-[#04c8bb] hover:text-[#92efe6] hover:border-[#92efe6] font-semibold hover:bg-inherit">
+                                    <Button onClick={toggleQRCode} className="bg-inherit border w-full border-[#04c8bb] text-[#04c8bb] hover:text-[#92efe6] hover:border-[#92efe6] font-semibold hover:bg-inherit text-sm sm:text-base">
                                         {showQRCode ? null : 'Show QR Code'}
                                     </Button>
                                 </div>
                             </div>
                         </div>
                     </CardContent>
-                    <CardFooter className="flex justify-end items-center">
-                        <p className="text-gray-400 text-sm">
+                    <CardFooter className="flex flex-col sm:flex-row justify-between sm:justify-end items-center gap-2 sm:gap-0">
+                        <p className="text-gray-400 text-xs sm:text-sm">
                             Expires in 24 hours
                         </p>
-                        <Button onClick={deleteFile} className="bg-[#04c8bb] text-sm px-1 h-6 text-white font-semibold hover:bg-[#92efe6] ml-4">
+                        <Button onClick={deleteFile} className="bg-[#04c8bb] text-xs sm:text-sm px-2 h-6 sm:ml-2 text-white font-semibold hover:bg-[#92efe6] w-full sm:w-auto">
                             Delete file
                         </Button>
                     </CardFooter>
                 </Card>
             </div>
             {showQRCode && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                    <div className="bg-inherit border-white border-2 p-6 rounded-lg">
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+                    <div className="bg-inherit border-white border-2 p-4 sm:p-6 rounded-lg w-full max-w-xs sm:max-w-md">
                         <QRCode
-                            value={window.location.href as string}
+                            value={window.location.href}
                             size={256}
                             style={{ height: "auto", maxWidth: "100%", width: "100%" }}
                             viewBox={`0 0 256 256`}
                         />
-                        <Button onClick={toggleQRCode} className="mt-4 w-full bg-[#04c8bb] text-white hover:bg-[#072522]">
+                        <Button onClick={toggleQRCode} className="mt-4 w-full bg-[#04c8bb] text-white hover:bg-[#072522] text-sm sm:text-base">
                             Close
                         </Button>
                     </div>
